f6a343aa9959062829c4701fa5db2bb40af4fc0a
biryeongtrain <biryeongtrain06@gmail.com>
2025-11-26 15:38:46 +0900

    feat : dynamic map code import from nucleoid

---
+++ src/main/java/kim/biryeong/ttt/world/TTTMap.java
@@ -0,0 +1,92 @@
+package kim.biryeong.ttt.world;
+
+import kim.biryeong.ttt.game.manager.GameManager;
+import kim.biryeong.ttt.world.gen.TemplateChunkGenerator;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.random.Xoroshiro128PlusPlusRandom;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.TeleportTarget;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import xyz.nucleoid.fantasy.Fantasy;
+import xyz.nucleoid.fantasy.RuntimeWorldConfig;
+import xyz.nucleoid.fantasy.util.GameRuleStore;
+import xyz.nucleoid.map_templates.MapTemplate;
+import xyz.nucleoid.map_templates.TemplateRegion;
+
+import java.util.List;
+
+public class TTTMap {
+    private static final Logger LOGGER = LoggerFactory.getLogger("TTT_Map");
+    private final MapTemplate template;
+    private final List<TemplateRegion> spawns;
+    private final Identifier instanceId;
+    private ServerWorld world;
+
+    public TTTMap(Identifier id, MapTemplate template) {
+        this.template = template;
+        this.spawns = template.getMetadata().getRegions("spawn").toList();
+        this.instanceId = id;
+        if (this.spawns.isEmpty()) {
+            throw new IllegalStateException("No spawn region found.");
+        }
+
+    }
+
+    public void generateWorld(MinecraftServer server) {
+        RuntimeWorldConfig config = new RuntimeWorldConfig()
+                .setGenerator(new TemplateChunkGenerator(server, this.template))
+                .setDifficulty(Difficulty.PEACEFUL);
+
+        var gameRules = config.getGameRules();
+        gameRules.set(GameRules.RANDOM_TICK_SPEED, 0);
+        setDefaultRule(gameRules, GameRules.DO_MOB_SPAWNING, false);
+        setDefaultRule(gameRules, GameRules.DO_DAYLIGHT_CYCLE, false);
+        setDefaultRule(gameRules, GameRules.DO_WEATHER_CYCLE, false);
+        setDefaultRule(gameRules, GameRules.ANNOUNCE_ADVANCEMENTS, false);
+        setDefaultRule(gameRules, GameRules.DO_ENTITY_DROPS, false);
+        setDefaultRule(gameRules, GameRules.DO_FIRE_TICK, false);
+        setDefaultRule(gameRules, GameRules.DO_MOB_LOOT, false);
+        setDefaultRule(gameRules, GameRules.PROJECTILES_CAN_BREAK_BLOCKS, false);
+        setDefaultRule(gameRules, GameRules.DO_VINES_SPREAD, false);
+        setDefaultRule(gameRules, GameRules.REDUCED_DEBUG_INFO, true);
+        setDefaultRule(gameRules, GameRules.SHOW_DEATH_MESSAGES, false);
+
+        var worldHandle = Fantasy.get(server).getOrOpenPersistentWorld(instanceId, config);
+        this.world = worldHandle.asWorld();
+
+        GameManager.getInstance().sendMessage("월드 준비 완료. 로드된 맵 : <green>%s</green>".formatted(instanceId.toString()));
+    }
+
+    public ServerWorld getWorld() {
+        return world;
+    }
+
+    public void spreadPlayers(List<ServerPlayerEntity> participants) {
+        participants.forEach(player -> {
+            Xoroshiro128PlusPlusRandom random = GameManager.getInstance().getRandom();
+            int index = random.nextInt(spawns.size());
+            var location = this.spawns.get(index).getBounds().sampleBlock(random);
+
+            player.teleportTo(new TeleportTarget(this.world, location.toCenterPos(), Vec3d.ZERO, player.getYaw(), player.getPitch(), TeleportTarget.NO_OP));
+        });
+    }
+
+    public void closeMap(MinecraftServer server) {
+        Fantasy fantasy = Fantasy.get(server);
+        fantasy.tickDeleteWorld(this.world);
+        this.world = null;
+
+    }
+
+    private static void setDefaultRule(GameRuleStore rules, GameRules.Key<GameRules.BooleanRule> key, boolean value) {
+        if (!rules.contains(key)) {
+            rules.set(key, value);
+        }
+    }
+}
---
+++ src/main/java/kim/biryeong/ttt/world/gen/GeneratorBlockSamples.java
@@ -0,0 +1,18 @@
+package kim.biryeong.ttt.world.gen;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.world.gen.chunk.VerticalBlockSample;
+
+import java.util.Arrays;
+
+public class GeneratorBlockSamples {
+    public static final VerticalBlockSample VOID = new VerticalBlockSample(0, new BlockState[0]);
+
+    public static VerticalBlockSample heightmap(int minY, int maxY, BlockState block) {
+        int height = maxY - minY + 1;
+
+        var sample = new BlockState[height];
+        Arrays.fill(sample, block);
+        return new VerticalBlockSample(minY, sample);
+    }
+}
